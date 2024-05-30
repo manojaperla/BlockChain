f6ec11f0ebef3b124a992dad92b080b7fe798a85
manojaperla <119960373+manojaperla@users.noreply.github.com>
2024-05-30 11:24:05 -0400

    Add files via upload

---
+++ FakeData.js
@@ -0,0 +1,37 @@
+ const UserList = [
+{
+    id:1,
+    name: "Sandy",
+    username: "Sandy",
+    age: 21,
+    nationality: "USA",
+    
+},
+{
+    id:2,
+    name: "Samuel",
+    username: "Legend",
+    age: 45,
+    nationality: "CHILI",
+    
+},
+{
+    id:3,
+    name: "Benazir",
+    username: "Ben",
+    age: 20,
+    nationality: "MEXICO",
+},
+{
+    id:4,
+    name: "Bhairavi",
+    username: "Bhairavi*20",
+    age: 20,
+    nationality: "INDIA",
+},
+
+];
+
+
+
+module.exports = {UserList};
